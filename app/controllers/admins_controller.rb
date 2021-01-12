649ce34c6bc7e380ba6862bd570470fbe48fde96
nllew <n.lewis.perc@gmail.com>
2021-01-12 13:13:59 -0600

    Edits to admins routes

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,11 +1,5 @@
 class AdminsController < ApplicationController
 
-  get '/admins/home' do
-    @admin = Admin.find(session[:admin_id])
-    erb :'/admins/home.html'
-  end
-
-  
   get "/login" do
     erb :login
   end
@@ -15,19 +9,19 @@ class AdminsController < ApplicationController
 
     if @admin && @admin.authenticate(params[:password])
       session[:admin_id] = @admin.id
-      redirect '/success'
+      redirect '/admins/home'
     else
       redirect '/login'
     end
   end
 
-  get "/success" do
-    if logged_in?
-			redirect "/admins/home"
-		else
-			redirect "/login"
-		end
-	end
+  # get "/success" do
+  #   if logged_in?
+	# 		redirect "/admins/home"
+	# 	else
+	# 		redirect "/login"
+	# 	end
+	# end
 
   get "/logout" do
     session.clear
@@ -37,5 +31,10 @@ class AdminsController < ApplicationController
   get "/failure" do
     erb :failure
   end
-  
+
+  get '/admins/home' do
+    @admin = Admin.find(session[:admin_id])
+    erb :'/admins/home.html'
+  end
+
 end
