cd0353a7feb4705afe04f842777cefb00852b028
nllew <n.lewis.perc@gmail.com>
2021-01-11 22:56:35 -0600

    moved some admin routes to application_controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,31 +5,4 @@ class AdminsController < ApplicationController
     erb :'/admins/home.html'
   end
 
-  get "/login" do
-    erb :login
-  end
-
-  post "/login" do
-    @admin = Admin.find_by(email: params[:email])
-
-    if @admin && @admin.authenticate(params[:password])
-      session[:admin_id] = @admin.id
-      redirect '/success'
-    else
-      redirect '/login'
-    end
-  end
-
-  get "/success" do
-    if logged_in?
-			redirect "/admins/home"
-		else
-			redirect "/login"
-		end
-	end
-
-  get "/logout" do
-    session.clear
-    redirect "/"
-  end
 end
