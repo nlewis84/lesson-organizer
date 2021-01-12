b9e5b2ca17a1ce406e1745708cd4cda54503eb16
nllew <n.lewis.perc@gmail.com>
2021-01-12 13:04:46 -0600

    Moved routes from app_contr to admins_contr

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,4 +5,37 @@ class AdminsController < ApplicationController
     erb :'/admins/home.html'
   end
 
+  
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    @admin = Admin.find_by(email: params[:email])
+
+    if @admin && @admin.authenticate(params[:password])
+      session[:admin_id] = @admin.id
+      redirect '/success'
+    else
+      redirect '/login'
+    end
+  end
+
+  get "/success" do
+    if logged_in?
+			redirect "/admins/home"
+		else
+			redirect "/login"
+		end
+	end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+  
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,38 +13,6 @@ class ApplicationController < Sinatra::Base
     redirect '/success'
   end
 
-  get "/login" do
-    erb :login
-  end
-
-  post "/login" do
-    @admin = Admin.find_by(email: params[:email])
-
-    if @admin && @admin.authenticate(params[:password])
-      session[:admin_id] = @admin.id
-      redirect '/success'
-    else
-      redirect '/login'
-    end
-  end
-
-  get "/success" do
-    if logged_in?
-			redirect "/admins/home"
-		else
-			redirect "/login"
-		end
-	end
-
-  get "/logout" do
-    session.clear
-    redirect "/"
-  end
-
-  get "/failure" do
-    erb :failure
-  end
-
   helpers do
     def logged_in?
       !!session[:admin_id]
