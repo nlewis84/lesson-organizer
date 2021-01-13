cc2ea964479309a00c52755957bf8b356ea2aa05
nllew <n.lewis.perc@gmail.com>
2021-01-12 21:03:53 -0600

    removed manual password digest for seeds

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,16 +1,7 @@
 class Teacher < ActiveRecord::Base
-    # has_secure_password
-    # validates :username, presence: true, uniqueness: true
-
 
     has_many :lessons
     has_many :students, through: :lessons
     belongs_to :admin
-
-    # # Returns the hash digest of the given string.
-    # def Teacher.digest(string)
-    #     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    #     BCrypt::Password.create(string, cost: cost)
-    # end
 
 end
